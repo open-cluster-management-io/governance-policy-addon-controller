2461aeb613823419beb258d3cf5efe0f4cd9370c
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-03-08 10:43:08 -0500

    Clean up E2E assertions
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- test/e2e/case1_framework_deployment_test.go
+++ test/e2e/case1_framework_deployment_test.go
@@ -704,23 +704,21 @@ func checkContainersAndAvailabilityInNamespace(cluster managedClusterConfig, clu
 
 	if startupProbeInCluster(clusterIdx) {
 		By(logPrefix + "verifying all replicas in framework deployment are available")
-		Eventually(func() bool {
+		Eventually(func(g Gomega) {
 			deploy := GetWithTimeout(
 				client, gvrDeployment, case1DeploymentName, namespace, true, 60,
 			)
 
 			replicas, found, err := unstructured.NestedInt64(deploy.Object, "status", "replicas")
-			if !found || err != nil {
-				return false
-			}
+			g.Expect(found).To(BeTrue(), "status.replicas should exist in the deployment")
+			g.Expect(err).ToNot(HaveOccurred())
 
 			available, found, err := unstructured.NestedInt64(deploy.Object, "status", "availableReplicas")
-			if !found || err != nil {
-				return false
-			}
+			g.Expect(found).To(BeTrue(), "status.availableReplicas should exist in the deployment")
+			g.Expect(err).ToNot(HaveOccurred())
 
-			return available == replicas
-		}, 240, 1).Should(Equal(true))
+			g.Expect(available).To(Equal(replicas), "available replicas should equal expected replicas")
+		}, 240, 1).Should(Succeed())
 	}
 
 	By(logPrefix + "verifying one framework pod is running")
