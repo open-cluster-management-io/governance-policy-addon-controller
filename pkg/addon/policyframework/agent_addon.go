d4cd84ff477a5f1caa920c319a407301315df2da
Justin Kulikauskas <jkulikau@redhat.com>
2026-09-08 13:44:21 -0400

    Directly handle ManagedClusterImageRegistry
    Previously, when a cluster was using a ManagedClusterImageRegistry, the
    images used by these addons were handled by the multiclusterhub-operator
    setting the adjusted image paths in the `values` annotation on the MCAO.
    However, to address CVE-2026-66793, that path was removed. As a result,
    disconnected environments that required a ManagedClusterImageRegistry
    were potentially broken.
    
    Now, the addon agents here directly read and apply the MCIR settings
    using a helper from the addon-framework library. This helper effectively
    enforces the image to be used from the environment variable when set,
    so the bespoke implementation before is no longer needed.
    
    Refs:
     - https://issues.redhat.com/browse/ACM-43984
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- pkg/addon/policyframework/agent_addon.go
+++ pkg/addon/policyframework/agent_addon.go
@@ -183,26 +183,32 @@ func GetAgentAddon(ctx context.Context, controllerContext *controllercmd.Control
 		Cluster().V1().ManagedClusters()
 	go clusterInformer.Informer().Run(ctx.Done())
 
+	aodcGetter := utils.NewAddOnDeploymentConfigGetter(addonClient)
+
+	valueFuncs := []addonfactory.GetValuesFunc{
+		getValuesFromAnnotations(clusterInformer.Lister()),
+		addonfactory.GetValuesFromAddonAnnotation,
+		addonfactory.GetAddOnDeploymentConfigValues(
+			utils.NewAddOnDeploymentConfigGetter(addonClient),
+			addonfactory.ToAddOnNodePlacementValues,
+			addonfactory.ToAddOnResourceRequirementsValues,
+			getValuesFromCustomizedVariableValues,
+		),
+		policyaddon.MandateValues,
+	}
+
+	imgFromEnv := os.Getenv("GOVERNANCE_POLICY_FRAMEWORK_ADDON_IMAGE")
+	if imgFromEnv != "" {
+		valueFuncs = append(valueFuncs, addonfactory.GetAgentImageValues(aodcGetter,
+			"global.imageOverrides.governance_policy_framework_addon", imgFromEnv))
+	}
+
 	return addonfactory.NewAgentAddonFactory(addonName, FS, "manifests/managedclusterchart").
 		WithConfigGVRs(utils.AddOnDeploymentConfigGVR).
-		WithGetValuesFuncs(
-			getValuesFromAnnotations(clusterInformer.Lister()),
-			addonfactory.GetValuesFromAddonAnnotation,
-			addonfactory.GetAddOnDeploymentConfigValues(
-				utils.NewAddOnDeploymentConfigGetter(addonClient),
-				addonfactory.ToAddOnNodePlacementValues,
-				addonfactory.ToAddOnResourceRequirementsValues,
-				getValuesFromCustomizedVariableValues,
-			),
-			policyaddon.MandateValues,
-			mandateImageFromEnv,
-		).
+		WithGetValuesFuncs(valueFuncs...).
 		WithManagedClusterClient(clusterClient).
 		WithAgentRegistrationOption(registrationOption).
-		WithAgentInstallNamespace(
-			policyaddon.
-				CommonAgentInstallNamespaceFromDeploymentConfigFunc(utils.NewAddOnDeploymentConfigGetter(addonClient)),
-		).
+		WithAgentInstallNamespace(policyaddon.CommonAgentInstallNamespaceFromDeploymentConfigFunc(aodcGetter)).
 		WithScheme(policyaddon.Scheme).
 		WithAgentHostedModeEnabledOption().
 		BuildHelmAgentAddon()
@@ -212,26 +218,4 @@ func GetAndAddAgent(
 	ctx context.Context, mgr addonmanager.AddonManager, controllerContext *controllercmd.ControllerContext,
 ) error {
 	return policyaddon.GetAndAddAgent(ctx, mgr, addonName, controllerContext, GetAgentAddon)
-}
-
-// mandateImageFromEnv ensures that if the environment variable for the image is
-// set to a non-empty value, that value is used in the chart.
-func mandateImageFromEnv(
-	_ *clusterv1.ManagedCluster,
-	_ *addonapiv1beta1.ManagedClusterAddOn,
-) (addonfactory.Values, error) {
-	values := addonfactory.Values{}
-
-	img := os.Getenv("GOVERNANCE_POLICY_FRAMEWORK_ADDON_IMAGE")
-	if img == "" {
-		return values, nil
-	}
-
-	values["global"] = map[string]any{
-		"imageOverrides": map[string]any{
-			"governance_policy_framework_addon": img,
-		},
-	}
-
-	return values, nil
 }
--- test/e2e/case1_framework_deployment_test.go
+++ test/e2e/case1_framework_deployment_test.go
@@ -5,6 +5,7 @@ package e2e
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"maps"
 	"time"
@@ -33,6 +34,9 @@ const (
 	case1MWPatch                         string = "../resources/manifestwork_add_patch.json"
 	ocmPolicyNs                          string = "open-cluster-management-policies"
 	deletionOrphanAnnotationKey          string = "addon.open-cluster-management.io/deletion-orphan"
+	imageRegistriesKey                   string = "open-cluster-management.io/image-registries"
+	imageRegistriesAnnotation            string = imageRegistriesKey + "=" +
+		`{"registries":[{"mirror":"quay.io:443/open-cluster-management","source":"quay.io/open-cluster-management"}]}`
 )
 
 var _ = Describe("Test framework deployment", func() {
@@ -278,6 +282,9 @@ var _ = Describe("Test framework deployment", func() {
 
 			checkContainersAndAvailability(ctx, cluster, i)
 
+			By(logPrefix + "verifying the default framework deployment is not using the mirror registry")
+			Eventually(getCase1DeploymentImage(c, cluster), 120, 1).ShouldNot(ContainSubstring("quay.io:443"))
+
 			By(logPrefix + "annotating the managedclusteraddon with the " + loggingLevelAnnotation + " annotation")
 			Kubectl(c, "annotate", "-n", cluster.clusterName, "-f", case1ManagedClusterAddOnCR, loggingLevelAnnotation)
 
@@ -291,6 +298,15 @@ var _ = Describe("Test framework deployment", func() {
 			By(logPrefix + "annotating the managedclusteraddon with the " + clientQPSAnnotation + " annotation")
 			Kubectl(c, "annotate", "-n", cluster.clusterName, "-f", case1ManagedClusterAddOnCR, clientQPSAnnotation)
 
+			By(logPrefix + "annotating the managedcluster with the " + imageRegistriesAnnotation + " annotation")
+			Kubectl(c, "annotate", "managedcluster", cluster.clusterName, "--overwrite", imageRegistriesAnnotation)
+
+			clusterName := cluster.clusterName
+
+			DeferCleanup(func(ctx context.Context) {
+				Kubectl(ctx, "annotate", "managedcluster", clusterName, imageRegistriesKey+"-")
+			})
+
 			checkArgs(ctx, cluster,
 				"--log-encoder=console",
 				"--log-level=8",
@@ -299,6 +315,9 @@ var _ = Describe("Test framework deployment", func() {
 				"--client-max-qps=50",
 				"--client-burst=111",
 			)
+
+			By(logPrefix + "verifying framework deployment is using the mirror registry")
+			Eventually(getCase1DeploymentImage(c, cluster), 120, 1).Should(ContainSubstring("quay.io:443"))
 
 			By(logPrefix + "removing the framework deployment when the ManagedClusterAddOn CR is removed")
 			Kubectl(c, "delete", "-n", cluster.clusterName, "-f", case1ManagedClusterAddOnCR, "--timeout=180s")
@@ -344,6 +363,9 @@ var _ = Describe("Test framework deployment", func() {
 				ctx, cluster.clusterClient, gvrNamespace, ocmPolicyNs, "", true, 60,
 			)
 			Expect(ns).ShouldNot(BeNil())
+
+			By(logPrefix + "verifying framework deployment is using the mirror registry")
+			Eventually(getCase1DeploymentImage(c, cluster), 120, 1).Should(ContainSubstring("quay.io:443"))
 
 			By(logPrefix + "removing the framework deployment when the ManagedClusterAddOn CR is removed")
 			Kubectl(c, "delete", "-n", cluster.clusterName, "-f", case1ManagedClusterAddOnCR, "--timeout=180s")
@@ -1081,4 +1103,50 @@ func installAddonInHostedMode(
 		ctx, &addon, metav1.CreateOptions{},
 	)
 	Expect(err).ToNot(HaveOccurred())
+}
+
+func getCase1DeploymentImage(ctx context.Context, cluster managedClusterConfig) func() (string, error) {
+	return func() (string, error) {
+		namespace := addonNamespace
+		client := cluster.clusterClient
+
+		if cluster.hostedOnHub {
+			namespace = "klusterlet-" + cluster.clusterName
+			client = managedClusterList[0].clusterClient
+		}
+
+		deploy := GetWithTimeout(ctx, client, gvrDeployment, case1DeploymentName, namespace, true, 60)
+		if deploy == nil {
+			return "", errors.New("Failed to get deployment")
+		}
+
+		conts, found, err := unstructured.NestedSlice(deploy.Object, "spec", "template", "spec", "containers")
+		if err != nil {
+			return "", err
+		}
+
+		if !found {
+			return "", errors.New("spec.template.spec.containers should exist in the deployment")
+		}
+
+		if len(conts) != 1 {
+			return "", errors.New("spec.template.spec.containers should only have 1 item")
+		}
+
+		cont, ok := conts[0].(map[string]any)
+		if !ok {
+			return "", errors.New("spec.template.spec.containers[0] should be a map[string]any")
+		}
+
+		img, found, err := unstructured.NestedString(cont, "image")
+		if err != nil {
+			return "", err
+		}
+
+		if !found {
+			return "", errors.New("spec.template.spec.containers[0].image should exist in the deployment")
+		}
+
+		return img, nil
+	}
 }
